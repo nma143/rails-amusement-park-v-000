bf0b50a971a3b1b435ded4cd59ffc917350b3a0e
Nikki A <nikki.apostolakis@gmail.com>
2019-02-25 22:29:05 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,10 +3,8 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-
-    if self.user.tickets >= self.attraction.tickets #enough tickets
-
-      if self.user.height >= self.attraction.min_height #tall enough
+    if self.user.height >= self.attraction.min_height #tall enough
+      if self.user.tickets >= self.attraction.tickets #enough tickets
         #enough tickets and tall enough -> take the ride
         self.user.update(
           :tickets => self.user.tickets - self.attraction.tickets,
@@ -16,11 +14,12 @@ class Ride < ActiveRecord::Base
         "Thanks for riding the #{self.attraction.name}!"
 
       else
-        "You are not tall enough to ride the #{self.attraction.name}"
+        "You do not have enough tickets to ride the #{self.attraction.name}"
       end
 
     else
-      "You do not have enough tickets to ride the #{self.attraction.name}"
+      "You are not tall enough to ride the #{self.attraction.name}"
+
     end
   end
 
