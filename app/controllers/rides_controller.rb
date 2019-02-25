e84ec18eb3fd993b5dffd84d93cdc5ff1b82f5fc
Nikki A <nikki.apostolakis@gmail.com>
2019-02-25 22:05:36 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,7 +3,6 @@ class RidesController < ApplicationController
   def new
     @ride = Ride.create(:user_id => current_user.id, :attraction_id => params[:attraction_id])
     @ride.save
-    raise @ride.user.inspect
     @message = @ride.take_ride
     redirect_to user_path(@ride.user, :message => @message)
   end
